2537374223cea79a7cb479136bc78ba97a108473
cykoravish <ravishbisht85@gmail.com>
2026-01-05 07:51:24 +0530

    type coersion add

--- basics/script.js
+++ basics/script.js
@@ -15,17 +15,27 @@
 
 // let a = 12;
 // let b = "13";
-
 // console.log(a+b) //concatination
 // console.log(a-b) //substraction
 // console.log(typeof (a+b)) //string
 // console.log(typeof (a-b)) //number
+// -----------------------------------------------
 
 // 3. sum and message
 // let a = 10;
 // let b = 20;
-
 // console.log("sum of 10 and 20 is " + a + b);
 // console.log("sum of 10 and 20 is ", (a + b)); // (bracket have more priority)
 // console.log(a + b + " is sum of 10 and 20")
+// -----------------------------------------------
+
+//4. type coersion
+// console.log(1 + "1");
+// console.log(1 - 1);
+// console.log(1 * "1");
+// console.log("1" / "1");
+// console.log("1" / 1);
+// -----------------------------------------------
+
+
 
