9656da968e1f507ea78982cc61165d04897d83cb
cykoravish <ravishbisht85@gmail.com>
2026-01-05 07:22:15 +0530

    dsa with js

---
+++ basics/script.js
@@ -0,0 +1,22 @@
+//0. variable declared with var can be used before declaration so we will use let or const
+// console.log(a);
+// var a;
+
+// 1. sum of two integer
+// let a = 10;
+// let b = 20;
+// console.log(a + b);
+// -----------------------------------------------
+
+//2. Relation between number and string 
+
+// "10" => string
+// 10 => number
+
+// let a = 12;
+// let b = "13";
+// console.log(a+b) //concatination
+// console.log(a-b) //substraction
+// console.log(typeof (a+b)) //string
+// console.log(typeof (a-b)) //number
+
