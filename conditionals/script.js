375d424f6c66d767a6f45c866b63d6db97803fce
cykoravish <ravishbisht85@gmail.com>
2026-01-06 22:54:30 +0530

    refactor: comment out voter and discount logic examples

--- conditionals/script.js
+++ conditionals/script.js
@@ -1,9 +1,27 @@
 // 1. valid voter
-const ans = Number(prompt("what is your age ?"));
-if (isNaN(ans)) {
-  alert("enter valid number");
-} else if (ans >= 18) {
-  alert("you can vote");
-} else {
-  alert("you can not vote");
-}
+// const ans = Number(prompt("what is your age ?"));
+// if (isNaN(ans)) {
+//   alert("enter valid number");
+// } else if (ans >= 18) {
+//   alert("you can vote");
+// } else {
+//   alert("you can not vote");
+// }
+//---------------------------------------------------
+
+// 2. shop discount
+// let amount = Number(prompt("What is the final amount ?"));
+// let dis = 0;
+// if (amount > 0 && amount <= 5000) {
+//   dis;
+// } else if (amount > 5000 && amount <= 7000) {
+//   dis = 5;
+// } else if (amount > 7000 && amount <= 9000) {
+//   dis = 10;
+// } else if (amount > 9000) {
+//   dis = 20;
+// }
+// console.log(amount - Math.floor((dis * amount) / 100));
+//---------------------------------------------------
+
+//3.
