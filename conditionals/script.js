7cfbf7ffa4c21af2ca78512f6d12e489cc5835b3
cykoravish <ravishbisht85@gmail.com>
2026-01-08 21:48:32 +0530

    switch case another example

--- conditionals/script.js
+++ conditionals/script.js
@@ -149,29 +149,37 @@
 //   : console.log("zero");
 
 //6. switch case
+// let day = 4;
+// switch (day) {
+//   case 1:
+//     console.log("monday");
+//     break;
+//   case 2:
+//     console.log("tuesday");
+//     break;
+//   case 3:
+//     console.log("wednesday");
+//     break;
+//   case 4:
+//     console.log("thursday");
+//     break;
+//   case 5:
+//     console.log("friday");
+//     break;
+//   case 6:
+//     console.log("saturday");
+//     break;
+//   case 7:
+//     console.log("sunday");
+//     break;
+// }
 
-let day = 4;
-
-switch (day) {
-  case 1:
-    console.log("monday");
+// another example
+switch (true) {
+  case 1 > -1:
+    console.log("wow");
     break;
-  case 2:
-    console.log("tuesday");
-    break;
-  case 3:
-    console.log("wednesday");
-    break;
-  case 4:
-    console.log("thursday");
-    break;
-  case 5:
-    console.log("friday");
-    break;
-  case 6:
-    console.log("saturday");
-    break;
-  case 7:
-    console.log("sunday");
+  case 2 > 1:
+    console.log("hehe");
     break;
 }
