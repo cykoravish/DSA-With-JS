1587927288e31eb6a050ff46f156174942ec7ca5
cykoravish <ravishbisht85@gmail.com>
2026-01-15 06:21:05 +0530

    print starts and install prompt-sync node package

---
+++ patternProgramming/script.js
@@ -0,0 +1,22 @@
+// console.log("hello")
+// console.log("world")
+
+//node js environment specific
+// process.stdout.write("hello")
+// process.stdout.write(" world")
+
+// let a = prompt("what is your name ?")
+// console.log(a);
+
+// node js specific 
+const prompt = require("prompt-sync")();
+
+// let a = prompt("what is your name ?");
+// process.stdout.write(a)
+
+// 1. print 5 starts on different lines 
+console.log("⭐")
+console.log("⭐")
+console.log("⭐")
+console.log("⭐")
+console.log("⭐")
