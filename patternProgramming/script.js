14a81e263813e93655382c779d057112c73a0cf0
cykoravish <ravishbisht85@gmail.com>
2026-01-15 06:41:52 +0530

    print star on user input

--- patternProgramming/script.js
+++ patternProgramming/script.js
@@ -8,22 +8,28 @@
 // let a = prompt("what is your name ?")
 // console.log(a);
 
-// node js specific 
+// node js specific
 const prompt = require("prompt-sync")();
 
 // let a = prompt("what is your name ?");
 // process.stdout.write(a)
 
-// 1. print 5 starts on different lines 
+// 1. print 5 starts on different lines
 // console.log("⭐")
 // console.log("⭐")
 // console.log("⭐")
 // console.log("⭐")
 // console.log("⭐")
 
-// 2. print 5 starts in same line 
+// 2. print 5 starts in same line
 // process.stdout.write("⭐")
 // process.stdout.write("⭐")
 // process.stdout.write("⭐")
 // process.stdout.write("⭐")
 // process.stdout.write("⭐")
+
+//3. print stars on user input
+const starCount = Number(prompt("enter the number of stars you wanna print ?"));
+for (let i = 0; i < starCount; i++) {
+  process.stdout.write("⭐");
+}
